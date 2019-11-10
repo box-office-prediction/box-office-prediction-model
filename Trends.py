9f6c4a7a2a413b7975338c4d38cfcfbaa2044c45
Manfredit <manfredit@my.easternct.edu>
2019-11-10 16:19:45 -0500

    Updating Trends.py

--- Trends.py
+++ Trends.py
@@ -6,27 +6,53 @@ Created on Sun Nov  3 11:53:08 2019
 """
 from pytrends.request import TrendReq
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 pytrend = TrendReq()
+p = 'C:/Users/deman/.spyder-py3/Senior Research/box-office-prediction-model/movie_titles2.txt'
+t = open('output.txt', 'w+')
+with open(p, 'r') as f:
+    list2 = []
+    for item in f:
+        number = 0 
+        while number < 1:
+            list2.append(str(item))
+            number += 1
+    #print(list2)
+    #list1 = ['Avengers: Endgame', 'Joker', 'It 2']
+    movies = []
+    x = 0
+    for i in list2:
+        scores = []
+        movies.append(i)
+        kw_list=['Twitter', i]
+        pytrend.build_payload(kw_list, timeframe = '2019-01-01 2019-11-04')
+        interest_over_time_df = pytrend.interest_over_time()
+        del interest_over_time_df['isPartial']
+        a = np.array(interest_over_time_df[i])
+        temp = 0
+        for i in a:
+            if i > temp:
+                temp = i
+                scores.append(temp)
+                
+        #t.write(str(temp))
+        #t.write(movies[x])
+        print(temp)
+        print(movies[x])    
+        
+        x += 1
+       
+    
+t.close()
 
-kw_list=['Avengers: Endgame', 'Joker', 'It 2','1234567']
-pytrend.build_payload(kw_list, timeframe = '2019-01-01 2019-11-01')
-
-
-
-
-#pytrend.get_historical_interest(kw_list, year_start=2019, month_start=10, day_start=1, year_end=2019, month_end=11, day_end=6,  cat=0, geo='', gprop='', sleep=0)
-
-interest_over_time_df = pytrend.interest_over_time()
-del interest_over_time_df['isPartial']
-a = np.array(interest_over_time_df['Avengers: Endgame'])
-jokr = np.array(interest_over_time_df['Joker'])
-it = np.array(interest_over_time_df['It 2'])
 
 
 
 
 
-print(interest_over_time_df) #interest_over_time_df
+#print(interest_over_time_df) 
+#avng = np.array(interest_over_time_df['Avengers: Endgame'])
+#jokr = np.array(interest_over_time_df['Joker'])
+#it = np.array(interest_over_time_df['It 2'])
